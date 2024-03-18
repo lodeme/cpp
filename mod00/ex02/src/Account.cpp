aa6c31fbf5a6f6106a470494175f82619b4ceaf2
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2024-03-18 16:46:19 +0100

    added account constructor

--- mod00/ex02/src/Account.cpp
+++ mod00/ex02/src/Account.cpp
@@ -13,7 +13,20 @@ void Account::_displayTimestamp(void)
 	time_t currentTime = time(0);
 	struct tm *localTime = localtime(&currentTime);
 	char buffer[20];
-	strftime(buffer, 20, "[%Y%m%d_%H%M%S]", localTime);
-	std::cout << buffer;
+	strftime(buffer, 20, "[%Y%m%d_%H%M%S] ", localTime);
+	std::cout << buffer << std::flush;
 }
 
+Account::Account(int deposit)
+{
+	_accountIndex = _nbAccounts;
+	_amount += deposit;
+	_nbAccounts++;
+	_totalAmount++;
+	_displayTimestamp();
+	std::cout
+		<< "index:" << _accountIndex
+		<< ";amount:" << _amount
+		<< ";created"
+		<< std::endl;
+}
