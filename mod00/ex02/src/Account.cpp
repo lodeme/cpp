3f860eb949488a34e99185394bb5ad64ba0103ef
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2024-03-20 14:06:22 +0100

    account file works

--- mod00/ex02/src/Account.cpp
+++ mod00/ex02/src/Account.cpp
@@ -21,6 +21,8 @@ Account::Account(int deposit)
 {
 	_accountIndex = _nbAccounts;
 	_amount += deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
 	_nbAccounts++;
 	_totalAmount++;
 	_displayTimestamp();
@@ -29,4 +31,75 @@ Account::Account(int deposit)
 		<< ";amount:" << _amount
 		<< ";created"
 		<< std::endl;
+}
+
+Account::~Account(void)
+{
+	_displayTimestamp();
+	std::cout
+		<< "index:" << _accountIndex
+		<< ";amount" << _amount
+		<< ";closed"
+		<< std::endl;
+}
+
+void Account::displayAccountsInfos(void)
+{
+	_displayTimestamp();
+	std::cout
+		<< "accounts:" << _nbAccounts
+		<< ";total:" << _totalAmount
+		<< ";deposits:" << _totalNbDeposits
+		<< ";withdrawals:" << _totalNbWithdrawals
+		<< std::endl;
+}
+void Account::displayStatus(void) const
+{
+	_displayTimestamp();
+	std::cout
+		<< "index:" << _accountIndex
+		<< ";amount:" << _amount
+		<< ";deposits:" << _nbDeposits
+		<< ";withdrawals:" << _nbWithdrawals
+		<< std::endl;
+}
+
+void Account::makeDeposit(int deposit)
+{
+	_nbDeposits++;
+	_totalNbDeposits++;
+	_totalAmount += deposit;
+	_displayTimestamp();
+	std::cout
+		<< "index:" << _accountIndex
+		<< ";p_amount:" << _amount
+		<< ";deposit:" << deposit
+		<< ";amount:" << _amount + deposit
+		<< ";nb_deposit:" << _nbDeposits
+		<< std::endl;
+	_amount += deposit;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+	std::cout
+		<< "index:" << _accountIndex
+		<< ";p_amount:" << _amount
+		<< ";withdrawal:"
+		<< std::flush;
+	if (withdrawal > _amount)
+	{
+		std::cout << "refused" << std::endl;
+		return (false);
+	}
+	std::cout
+		<< withdrawal
+		<< ";amount:" << _amount - withdrawal
+		<< ";nb_deposit:" << _nbDeposits
+		<< std::endl;
+	_amount -= withdrawal;
+	_totalAmount -= withdrawal;
+	_nbWithdrawals++;
+	_totalNbWithdrawals++;
+	return (true);
 }
