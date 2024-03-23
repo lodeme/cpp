6b2880ebd6c26c7661cb53d3043c0961f80a93a1
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2024-03-23 01:52:49 +0100

    mod01/ex02

--- mod01/ex02/main.cpp
+++ mod01/ex02/main.cpp
@@ -1,3 +1,18 @@
+#include <string>
+#include <iostream>
 
+int main (void) {
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
 
-std::string 
+	std::cout << "Address of the string variable: " << &str << std::endl;
+	std::cout << "Address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of the string variable: " << str << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+
+	return EXIT_SUCCESS;
+}
