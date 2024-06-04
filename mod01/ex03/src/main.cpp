7ffd79db51d04d37a7f6570ffa37898440307fbf
lodemetz <lodemetz@student.42berlin.de>
2024-06-04 11:15:03 +0000

    improved tests

--- mod01/ex03/src/main.cpp
+++ mod01/ex03/src/main.cpp
@@ -1,24 +1,27 @@
 #include "Weapon.hpp"
 #include "HumanA.hpp"
 #include "HumanB.hpp"
+#include <iostream>
 
 int main()
 {
 	{
-	Weapon club = Weapon("crude spiked club");
-	HumanA bob("Bob", club);
-	bob.attack();
-	club.setType("some other type of club");
-	bob.attack();
+    Weapon club = Weapon("crude spiked club");
+    std::cout << "Weapon " << club.getType() << " created" << std::endl;
+    HumanA bob("Bob", club);
+    bob.attack();
+    club.setType("some other type of club");
+    bob.attack();
 	}
 
 	{
-	Weapon club = Weapon("crude spiked club");
-	HumanB jim("Jim");
-	jim.setWeapon(&club);
-	jim.attack();
-	club.setType("some other type of club");
-	jim.attack();
+    Weapon club = Weapon("other crude spiked club");
+    std::cout << "Weapon " << club.getType() << " created" << std::endl;
+    HumanB jim("Jim");
+    jim.setWeapon(&club);
+    jim.attack();
+    club.setType("some new other type of club");
+    jim.attack();
 	}
 
 	return 0;
