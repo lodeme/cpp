fa2eb3559fe2ca28e87f8ee88a68ddf9f1afe49f
lodemetz <lodemetz@student.42berlin.de>
2024-06-05 18:46:01 +0000

    init ex01 mod 04

---
+++ mod04/ex01/include/Dog.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <Animal.hpp>
+#include <Brain.hpp>
+
+class Dog : public Animal {
+  private:
+    Brain* _brain;
+
+  protected:
+    std::string _type;
+
+  public:
+    Dog(void);
+    ~Dog(void);
+    Dog(const Dog& other);
+    Dog& operator=(const Dog& other);
+
+    const std::string& getType(void) const;
+    void makeSound(void) const;
+};
+
