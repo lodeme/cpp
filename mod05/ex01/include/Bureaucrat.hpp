9a1b985859902a607f02813c6513eca02abf561f
lodemetz <lodemetz@student.42berlin.de>
2024-06-25 15:50:42 +0000

    make progress on forms / still a bit left to do

---
+++ mod05/ex01/include/Bureaucrat.hpp
@@ -0,0 +1,33 @@
+#include <string>
+
+class Bureaucrat {
+  private:
+    const std::string _name;
+    int _grade;
+
+  public:
+    // canonical form
+    Bureaucrat(void);
+    ~Bureaucrat(void);
+    Bureaucrat(const std::string& name, int grade);
+    Bureaucrat& operator=(const Bureaucrat& other);
+  
+    // getters and setters
+    const std::string& getName(void) const;
+    int getGrade(void) const;
+    void incrementGrade(void);
+    void decrementGrade(void);
+    
+    // Exceptions handling
+    class GradeTooHighException: public std::exception {
+      public:
+        virtual const char* what(void) const throw();
+    };
+    
+    class GradeTooLowException: public std::exception {
+      public:
+        virtual const char* what(void) const throw();
+    };
+};
+
+std::ostream &operator<<(std::ostream &str, Bureaucrat const &b);
