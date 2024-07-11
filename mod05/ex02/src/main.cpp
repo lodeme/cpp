b095cf556dc86f5243d64aa4d1d19f04a67807f4
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2024-07-12 01:27:59 +0200

    remove redundant inclusion

--- mod05/ex02/src/main.cpp
+++ mod05/ex02/src/main.cpp
@@ -3,7 +3,6 @@
 #include <Bureaucrat.hpp>
 #include <ShrubberyCreationForm.hpp>
 #include <RobotomyRequestForm.hpp>
-#include "RobotomyRequestForm.hpp"
 // #include "PresidentialPardonForm.hpp"
 
 int main( void )
